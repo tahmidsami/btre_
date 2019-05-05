b3853db9112669a8932c20a82d03af350a9454c7
Tahmid Sami <mtsami7264@gmail.com>
2019-05-06 02:53:19 +0600

    dynamicly done home and about page

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listings
+from realtors.models import Realtor
+
 
 def index(request):
-    return render(request, 'pages/index.html')
+    allListings = Listings.objects.order_by('-list_date').filter(is_published = True)[:3]
+
+    context = {
+        'allListings': allListings
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    allRealtors = Realtor.objects.order_by('hire_date')
+
+    mvp_realtors = Realtor.objects.all().filter(is_mvp = True)
+
+    context = {
+        'allRealtors': allRealtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
