0dc6fb4665ce6e239bf05111b6a350728a7f9296
Tahmid Sami <mtsami7264@gmail.com>
2019-05-05 19:38:17 +0600

    everything is fine

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Listings
 
-admin.site.register(Listings)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor', 'list_date')
+    #list_editable = ('is_published',)
+    search_fields = ('title', 'discription', 'price', 'city', 'state', 'zipcode', 'address')
+    list_per_page = 25
+
+admin.site.register(Listings, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Realtor
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    list_filter = ('name', 'hire_date')
+    #list_editable = ('is_published',)
+    search_fields = ('name', 'discription', 'email')
+    list_per_page = 25
+
+admin.site.register(Realtor, RealtorAdmin)
