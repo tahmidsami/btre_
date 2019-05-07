b6e0c27a3f193c4d3af7cf89aeae312e359362ef
Tahmid Sami <mtsami7264@gmail.com>
2019-05-07 18:03:32 +0600

    login successfull

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
 
 def login(request):
     if request.method == 'POST':
         # Login User
-        messages.error(request, 'Tessting error message')
-        return redirect('login')
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username = username, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Login Successfull!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Username or password is incorrect')
+            return redirect('login')
+
     else:
         return render(request, 'accounts/login.html')
 
